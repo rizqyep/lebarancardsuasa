1118848917b2e1dc6746c0b92494794af7693cb3
Rizqy EP <rizqyepr@gmail.com>
2022-04-29 02:35:54 +0700

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+let senderName = "";
+let additionalText = "";
+
+let senderNameInput = document.getElementById("senderName");
+let additionalTextInput = document.getElementById("additionalText");
+
+let senderNameTarget = document.getElementById("senderNameTarget");
+let additionalTextTarget = document.getElementById("additionalTextTarget");
+let lebaranCardResult = document.getElementById("lebaranCardResult");
+let resultModalBtn = document.getElementById("resultModalBtn");
+let choosenSrc = "";
+
+let loadingText = document.getElementById("loadingText")
+
+
+
+window.addEventListener('load', () => {
+    loadingText.hidden = true;
+    resultModalBtn.disabled = true;
+})
+
+let noPictureSelected = true;
+resultModalBtn.addEventListener('click', () => {
+    if (noPictureSelected) {
+        alert("Silahkan pilih gambar terlebih dahulu!");
+        return
+    } else {
+        senderName = senderNameInput.value;
+        additionalText = additionalTextInput.value;
+
+        senderNameTarget.innerText = senderName;
+        additionalTextTarget.innerText = additionalText;
+    }
+})
+
+function htmlToElement(html) {
+    let temp = document.createElement('div');
+    temp.innerHTML = html;
+    return temp;
+}
+
+
+let downloadBtn = document.getElementById("downloadBtn");
+
+downloadBtn.addEventListener('click', () => {
+
+
+    loadingText.hidden = false;
+    var html = `
+                <div id="lebaranCardResult">
+                    <img src=${choosenSrc} alt="LebaranCard Image" id="lebaranCardImageResult">
+                    <p className="text-center" id="senderNameTargetResult">${senderName}</p>
+                    <p className="text-center" id="additionalTextTargetResult">${additionalText}</p>
+                </div>`;
+    var tempResult = document.getElementById("tempResult");
+    tempResult.innerHTML = html;
+
+    var node = document.getElementById("lebaranCardResult")
+
+
+    htmlToImage.toPng(node)
+        .then(function(dataUrl) {
+            download(dataUrl, `Kartu Lebaran - ${senderName}.png`);
+            tempResult.innerHTML = ""
+            loadingText.hidden = true;
+
+
+        });
+})
+
+
+let lebaranCardOptions = document.querySelectorAll(".lebaranCardOption");
+
+function updateSelectedId(id) {
+    lebaranCardOptions.forEach((option) => {
+        if (option.id == id) {
+            option.classList.add("selectedOption")
+        } else {
+            option.classList.remove("selectedOption")
+        }
+    })
+}
+
+
+
+let lebaranCard = document.getElementById("lebaranCard");
+
+let lebaranCardImage = document.getElementById("lebaranCardImage");
+
+lebaranCardOptions.forEach((option) => {
+    option.addEventListener('click', (e) => {
+
+
+        lebaranCardImage.src = option.src
+        choosenSrc = option.src;
+
+
+
+
+        updateSelectedId(e.target.id);
+        noPictureSelected = false;
+        resultModalBtn.disabled = false;
+
+    })
+})
